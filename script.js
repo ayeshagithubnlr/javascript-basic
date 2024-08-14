92d9cc31533901dea5bfb6134ce98c5e7be391ad
shaik ayesha <ayishunlr.com>
2024-08-14 12:53:41 +0530

    file added

---
+++ script.js
@@ -0,0 +1,7 @@
+function f1()
+{
+let name=document.getElementById("username").value
+let password=document.getElementById("password").value
+document.write("username : "+name+"<br>")
+document.write("password : "+password+"<br>")
+}
